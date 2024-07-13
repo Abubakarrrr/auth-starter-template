7a6f96266c1faf3990f6f82ede2e6985e3e9c91a
Abubakar <abubakkeramjad10@gmail.com>
2024-07-13 19:48:00 +0500

    configured react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import NotFound from "./pages/NotFound"
 
 function App() {
 
-  return (
-    <>
-      <div className='bg-orange-400'>hello world</div>
-    </>
-  )
+  return <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/sign-in" element={<SignIn />} />
+    <Route path="/sign-up" element={<SignUp />} />
+    <Route path="/profile" element={<Profile />} />
+    <Route path="*" element={<NotFound />} />
+  </Routes>
+
 }
 
 export default App
---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div className='bg-black'>SignUp</div>
+  )
+}
+
+export default SignUp
