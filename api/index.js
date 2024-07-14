00d953f5a53a6ba6cda21c80c049baa2fb0d2246
Abubakar <abubakkeramjad10@gmail.com>
2024-07-14 10:17:06 +0500

    mongodb connection

--- api/index.js
+++ api/index.js
@@ -1,9 +1,19 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config()
+const PORT = process.env.PORT
+const URI = process.env.URI
+
+mongoose.connect(URI).then(() => {
+    console.log("mongodb connected successfully")
+}).catch((err)=> {
+     console.log(err);
+})
 
 const app = express();
 
 
-const PORT = 8080
 app.listen(PORT, () => {
     console.log(`Server listening on PORT ${PORT}`)
 })
