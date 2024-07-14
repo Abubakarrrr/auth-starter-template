5887af58eda39d91c9a3ef17320893af52a433c8
Abubakar <abubakkeramjad10@gmail.com>
2024-07-14 09:48:25 +0500

    backend setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,25 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from './pages/Home'
 import About from './pages/About'
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound"
+import Header from "./components/Header";
 
 function App() {
 
-  return <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/sign-in" element={<SignIn />} />
-    <Route path="/sign-up" element={<SignUp />} />
-    <Route path="/profile" element={<Profile />} />
-    <Route path="*" element={<NotFound />} />
-  </Routes>
+  return <div className="">
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+  </div>
 
 }
 
