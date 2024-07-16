2257be61cf5be79f2b1c7a93958bf6ee14615b81
Abubakar <abubakkeramjad10@gmail.com>
2024-07-16 16:02:30 +0500

    added google auth

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { signInStart, signInFailure, signInSuccess } from '../redux/user/userSlice'
 import { useDispatch, useSelector } from 'react-redux'
+import OAuth from '../components/OAuth'
 
 const SignIn = () => {
 
@@ -64,6 +65,7 @@ const SignIn = () => {
             disabled={loading}
             className='bg-slate-700 text-white p-3 rounded-lg  hover:opacity-95 disabled:opacity-80'
           > {loading ? 'Loading...' : 'Sign In'}</button>
+          <OAuth/>
 
         </form>
 
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import OAuth from '../components/OAuth'
 
 const SignUp = () => {
 
@@ -71,6 +72,8 @@ const SignUp = () => {
             disabled={loading}
             className='bg-slate-700 text-white p-3 rounded-lg  hover:opacity-95 disabled:opacity-80'
           > {loading ? 'Loading...' : 'Sign Up'}</button>
+
+          <OAuth/>
 
         </form>
 
